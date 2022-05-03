80eeb019a5883fba396b19af3e2fa8b0c488c954
Sean Hudgston <shudgst@us.ibm.com>
2022-05-03 16:55:53 -0400

    Simplify

--- python/65_merge_intervals.py
+++ python/65_merge_intervals.py
@@ -10,41 +10,19 @@ class Solution:
         for i in range(1, len(intervals)):
             a = merged[-1]
             b = intervals[i]
-            result = merge_interval(a, b)
 
-            if result:
-                merged[-1] = result
+            if a[1] >= b[0]:
+                merged[-1] = [a[0], max(a[1], b[1])]
             else:
                 merged.append(b)
 
         return merged
 
 
-def merge_interval(a: List[List[int]], b: List[List[int]]) -> List[List[int]]:
-    """
-    Retern a merged interval if two intervals can be merged,
-    or an empty list if not
-    """
-    if a[1] >= b[0] or b[0] <= a[0] and b[1] >= a[1]:
-        result = [min(a[0], b[0]), max(a[1], b[1])]
-        return result
-
-    return []
-
-
 print(Solution().merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
-
 print(Solution().merge([[1, 4], [4, 5]]))
-
 print(Solution().merge([[1, 3]]))
-
 print(Solution().merge([[1, 4], [5, 6]]))
-
 print(Solution().merge([[1, 4], [0, 4]]))
-
 print(Solution().merge([[1, 4], [0, 0]]))
-
-
-# [[2,3],[4,5],[6,7],[8,9],[1,10]] => 1, 10
-
 print(Solution().merge([[2, 3], [4, 5], [6, 7], [8, 9], [1, 10]]))
