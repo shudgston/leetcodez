152e41ccc5e2a2dc6bdfb1dce26cc83455390c3b
Sean Hudgston <shudgst@us.ibm.com>
2022-05-02 20:24:37 -0400

    Fix a bug

--- python/121_best_time_to_buy_and_sell_stock.py
+++ python/121_best_time_to_buy_and_sell_stock.py
@@ -1,4 +1,7 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+from typing import List
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         left = 0
@@ -6,16 +9,14 @@ class Solution:
         max_profit = 0
 
         while right < len(prices):
-            profit = prices[right] - prices[left]
-
-            if profit < 0:
-                # A loss, move ahead
-                left += 1
-                right += 1
-            else:
-                # A profit
+            if prices[left] < prices[right]:
+                # A profit. Keep L in place and move R regardless
+                profit = prices[right] - prices[left]
                 max_profit = max(max_profit, profit)
-                # Keep L where it is and move R
-                right += 1
+            else:
+                # Move L to the new low point
+                left = right
+
+            right += 1
 
         return max_profit
