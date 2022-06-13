defb022de8066c6a4ad5b885078f1a1439000982
Sean Hudgston <shudgst@us.ibm.com>
2022-06-13 17:30:51 -0400

    longest substring without repetition

---
+++ python/3_longest_substring_without_repeating_chars.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = set()
+        left = 0
+        longest = 0
+
+        for right in range(len(s)):
+            # A duplicate was found. Remove s[left] from set of seen chars,
+            # and advance the left pointer
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+
+            seen.add(s[right])
+            longest = max(longest, (right - left) + 1)
+
+        return longest
+
+
+strings = ["abcabcbb", "bbbbbbbb", "pwwkew", " ", "au"]
+
+for s in strings:
+    print(f"input: '{s}'")
+    print(Solution().lengthOfLongestSubstring(s))
