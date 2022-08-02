aab314b472a3fea5e14944b880ac180cd368cb26
Sean Hudgston <shudgst@us.ibm.com>
2022-08-01 21:25:19 -0400

    cleanup 271

--- python/271_encode_decode_strings.py
+++ python/271_encode_decode_strings.py
@@ -2,38 +2,43 @@
 
 from typing import List
 
-DELIM = "%"
+DELIM = "#"
 
 
 class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string."""
-        result = []
-        for word in strs:
-            wordlen = len(word)
-            result.append(str(wordlen) + DELIM + word)
+        res = ""
 
-        return "".join(result)
+        for s in strs:
+            res += str(len(s)) + DELIM + s
+            # "Hello" -> "5#Hello"
+
+        return res
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings."""
-        result = []
-        i = 0
+        l = 0
+        res = []
 
-        while i < len(s):
-            j = i
+        while l < len(s):
+            # "5#Hello5#World"
+            r = l
 
-            while s[j] != DELIM:
-                j += 1
+            while s[r] != DELIM:
+                r += 1
 
-            word_len = int(s[i:j])
-            end_of_word = j + 1 + word_len
-            result.append(s[j + 1 : end_of_word])
-            i = end_of_word
+            wordlen = int(s[l:r])
+            wordend = r + wordlen + 1
+            line = s[r + 1 : wordend]
+            res.append(line)
+            l = wordend
 
-        return result
+        return res
 
 
 # Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
 codec = Codec()
 print(codec.decode(codec.encode(["Hellooooo0o0o0o0o0o0o0o0ooX", "World"])))
