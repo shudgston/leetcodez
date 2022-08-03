dcb718e4f1636e7fd3a3444d323397c48cae856b
Sean Hudgston <shudgst@us.ibm.com>
2022-08-02 21:56:44 -0400

    cleanup and explain 347

--- python/347_top_k_frequent_elements.py
+++ python/347_top_k_frequent_elements.py
@@ -1,41 +1,39 @@
-from collections import Counter, defaultdict
+from collections import Counter
 from typing import List
 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+        count the freq of each n in nums
+        Counter({1: 3, 2: 2, 3: 1})
+
+        Create a list "buckets" of equal length to nums.
+        - each index in the list maps to the freq. count, i.e. the value of each Counter item
+        - [[], [3], [2], [1], [], []]
+            0   1    2.   3.   4   5
+        - iterate backwards through buckets
+
+        """
         count = Counter(nums)
-        bucket = [[] for _ in range(len(nums) + 1)]
+        # +1 accounts for when all numbers are the same
+        buckets = [[] for k in range(len(nums) + 1)]
+        res = []
 
-        for key, val in count.items():
-            bucket[val].append(key)
+        for key, ct in count.items():
+            buckets[ct].append(key)
 
-        results = []
+        # iterate backwards through buckets
+        #   iterate through each list in buckets, appending until length k is reached
+        for i in range(len(buckets) - 1, 0, -1):
+            for j in buckets[i]:
+                res.append(j)
+                if len(res) == k:
+                    return res
 
-        # count backwards
-        for i in range(len(bucket) - 1, 0, -1):
-            for n in bucket[i]:
-                results.append(n)
-
-                if len(results) == k:
-                    return results
-
-        return results
-
-
-def count_and_zip(nums, k):
-    count = Counter(nums)
-    res = sorted(
-        [x for x in zip(count.values(), count.keys())],
-        key=lambda x: x[0],
-        reverse=True,
-    )
-
-    return [x[1] for x in res][:k]
+        return res
 
 
 print(">>>", Solution().topKFrequent([1, 1, 1, 2, 2, 3], k=2))
-
 print(">>>", Solution().topKFrequent([3, 0, 1, 0], k=1))
-
 print(">>>", Solution().topKFrequent([-1, -1], k=1))
